bde6cdd1c8159804312f03162362a410a7212bea
Jesse Hunter <jjsbizarreadventure@yahoo.com>
2023-04-28 10:38:25 -0500

    finishing the idea of the backend

--- server/app.py
+++ server/app.py
@@ -4,6 +4,15 @@ from flask_restful import Api, Resource
 from config import app, db, api
 from models import *
 
+
+#TODO: remember that we need seperate gets that get get a list of Groups by User/Username and by Character
+#TODO: how to differentiate the User ID in Groups from the User id's related to the characters. Does it even matter due to scope?
+#TODO:
+
+
+
+
+
 class Home(Resource):
     def get(self):
         return make_response("API is running", 200)
@@ -32,6 +41,54 @@ class User(Resource):
         except Exception as e:
             db.session.rollback()
             return make_response( { 'Error' : str(e) }, 422 )
+
+api.add_resource(User, '/users')
+
+
+
+
+
+
+class UserById(Resource):
+    def get(self, id):
+        pass
+
+class Character(Resource):
+    def get(self):
+        pass
+
+class CharacterById(Resource):
+    def get(self, id):
+        pass
+
+
+class Group(Resource):
+    def get(self):
+        pass
+
+
+class GroupById(Resource):
+    def get(self, id):
+        pass
+
+class CharacterGroup(Resource):
+    def get(self):
+        pass
+
+
+class CharacterGroupById(Resource):
+    def get(self, id):
+        pass
+
+class CharacterGroupByUser(Resource):
+    def get(self, id):
+        pass
+
+class CharacterGroupByCharacter(Resource):
+    def get(self, id):
+        pass
+
+
 
 
 
